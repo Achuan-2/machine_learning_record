84b90ffc5b118ea324ca3d0271d25acd8fb60b66
Achuan-2 <achuan-2@outlook.com>
2022-08-10 15:25:48 +0800

    :memo: 05.激活函数：给机器注入灵魂

--- Ele实验室_小白也能听懂的人工智能原理/code/utils/dataset.py
+++ Ele实验室_小白也能听懂的人工智能原理/code/utils/dataset.py
@@ -11,3 +11,14 @@ def get_beans2(counts):
 	xs = np.sort(xs)
 	ys = np.array([(0.7*x+(0.5-np.random.rand())/5+0.5) for x in xs])
 	return xs,ys
+
+def get_beans3(counts):
+	xs = np.random.rand(counts)
+	xs = np.sort(xs)
+	ys = np.zeros(counts)
+	for i in range(counts):
+		x = xs[i]
+		yi = 0.7*x+(0.5-np.random.rand())/50+0.5
+		if yi > 0.8:
+			ys[i] = 1
+	return xs,ys
